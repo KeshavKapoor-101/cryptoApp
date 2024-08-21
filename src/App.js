7c46d0f380bd3ffbe96b4e5f84b9b8ab1a3bb6ec
KeshavKapoor-101 <keshavkapoor365@gmail.com>
2024-08-22 01:06:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+import {Routes ,Route} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Coin from './pages/Coin/Coin'
+import Footer from './components/footer/Footer'
+
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/coin/:coinId' element={<Coin/>}/>
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
